6c15a2e1d62e7c5816e3be424d12eee1f0d2e00e
Leime <67313990+tjwhang@users.noreply.github.com>
2025-03-04 19:51:26 +0900

    Update makepdf.bat

--- makepdf.bat
+++ makepdf.bat
@@ -1,9 +1,10 @@
 @title Build Thesis pdf
 @echo off
+set /p md = Name of markdown file to render: 
 set /p title= Input the output pdf file name: 
 
 echo Building %title%.pdf...
-pandoc Thesis.md -o %title%.pdf --pdf-engine=xelatex
+pandoc %md%.md -o %title%.pdf --pdf-engine=xelatex
 echo Operation complete.
 
 set \p key=Press any key to continue...
